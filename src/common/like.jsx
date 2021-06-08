82ff936fe8d4a05d40350642c0c59d400475c718
Somade Daniel <Flexsidee@gmail.com>
2021-06-08 17:54:52 +0100

    Coverted the Like component from a Class to a Stateless Functional component
    It only has the render method so there is no need for the Class Component

--- src/common/like.jsx
+++ src/common/like.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 
-class Like extends Component {
-    state = {  }
-    render() { 
-        let classes = 'fa fa-heart';
-        classes += (this.props.liked) ? '' : '-o';
-        return (  
-            <i onClick={this.props.onClick} style={{cursor: 'pointer'}}  className={classes} aria-hidden='true'></i>
-        );
-    }
+const Like = (props) => {
+    let classes = 'fa fa-heart';
+    classes += (props.liked) ? '' : '-o';
+    return ( 
+        <i onClick={props.onClick} style={{cursor: 'pointer'}}  className={classes} aria-hidden='true'></i>
+     );
 }
  
 export default Like;
