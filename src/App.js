3b35c4df8836bedbad6e6679fecc949643910324
Somade Daniel <Flexsidee@gmail.com>
2021-06-18 00:11:18 +0100

    Added routing to the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {Route, Redirect, Switch} from 'react-router-dom';
 import Movies from './components/movies';
+import NotFound from './components/notFound';
+import Customers from './components/customers';
+import rentals from './components/rentals';
 
 function App() {
   return (
     <main className='container'>
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={rentals} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from='/' exact to='/movies' />
+        <Redirect to='/not-found' />
+      </Switch>
     </main>
   );
 }
