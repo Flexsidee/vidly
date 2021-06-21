d2dde881cf3f35013dfb138b35a59409b41cf1f9
Somade Daniel <Flexsidee@gmail.com>
2021-06-21 20:21:49 +0100

    Updated the LoginForm validation using Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 import Input from './common/input';
 
 class LoginForm extends Component {
@@ -10,16 +11,21 @@ class LoginForm extends Component {
         errors: {}
     }
 
-    validate = () =>{
-        const errors = {}
-        const {account} =this.state;
-        
-        if(account.username.trim() === '') 
-            errors.username = 'Username is required';
-        if(account.password.trim() === '') 
-            errors.password = 'Password is required';
+    schema = {
+        username: Joi.string().required().label('Username'),
+        password: Joi.string().required().label('Password')
+    }
 
-        return (Object.keys(errors).length === 0 ) ? null : errors;
+    validate = () =>{
+        const options = {abortEarly: false};
+        const {error} = Joi.validate(this.state.account, this.schema, options);
+        
+        if(!error) return null;
+
+        const errors = {};
+        for ( let item of error.details)
+            errors[item.path[0]] = item.message;
+        return errors;
     }
     
     validateProperty = ({name, value}) =>{
