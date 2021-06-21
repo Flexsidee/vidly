d4893764b4c4ddaec91c51f29275b1b4c3f4f6d4
Somade Daniel <Flexsidee@gmail.com>
2021-06-21 17:05:00 +0100

    Updating the Login Form validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -22,6 +22,15 @@ class LoginForm extends Component {
         return (Object.keys(errors).length === 0 ) ? null : errors;
     }
     
+    validateProperty = ({name, value}) =>{
+        if(name === 'username'){
+            if(value.trim() === '') return 'Username is Required';
+        }
+        if(name === 'password'){
+            if(value.trim() === '') return 'Password is Required';
+        }
+    }
+
     handleSubmit = e =>{
         e.preventDefault();
 
@@ -33,9 +42,15 @@ class LoginForm extends Component {
     }
 
     handleChange = ({currentTarget: input}) =>{
+        const errors= {...this.state.errors};
+        const errorMessage = this.validateProperty(input);
+        if(errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
         const account = {...this.state.account};
         account[input.name] = input.value;
-        this.setState({account});
+
+        this.setState({account, errors});
     }
 
     render() {
