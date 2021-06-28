0c76822245cb24de2407017941957dc87d0be569
Somade Daniel <Flexsidee@gmail.com>
2021-06-28 21:51:01 +0100

    Fixed the link error in the pagination component

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -12,8 +12,8 @@ const Pagination = ({itemsCount, pageSize, onPageChange, currentPage} ) => {
         <nav>
             <ul className="pagination justify-content-center">
                 {pages.map(page => (<li key={page} className={(page === currentPage) ? 'page-item active' : 'page-item'}>
-                                <a className="page-link"
-                                onClick={() => onPageChange(page) }>{page}</a>
+                                <button className="page-link"
+                                onClick={() => onPageChange(page) }>{page}</button>
                                 </li>))}
                 
             </ul>
