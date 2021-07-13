2cfaa73b1a8b902e74f74db158fc2c3354b1e702
Somade Daniel <Flexsidee@gmail.com>
2021-07-13 18:10:11 +0100

    Connected the MovieForm to the backend

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Form from "./common/form";
 import Joi from "joi-browser";
-import { getMovie, saveMovie } from "../services/fakeMovieService";
-import { getGenres } from "./../services/fakeGenreService";
+import { getMovie, saveMovie } from "../services/movieService";
+import { getGenres } from "./../services/genreService";
 
 class MovieForm extends Form {
   state = {
@@ -28,17 +28,26 @@ class MovieForm extends Form {
     dailyRentalRate: Joi.number().required().min(0).max(10).label("Rate"),
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async populateGenres() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
+  async populateMovie() {
+    try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        return this.props.history.replace("/not-found");
+    }
+  }
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
-
-    this.setState({ data: this.mapToViewModel(movie) });
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
@@ -51,8 +60,8 @@ class MovieForm extends Form {
     };
   }
 
-  doSubmit = () => {
-    saveMovie(this.state.data);
+  doSubmit = async () => {
+    await saveMovie(this.state.data);
 
     this.props.history.push("/movies");
   };
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,5 @@
 import http from "./httpService";
-import {apiUrl} from '../config.json';
+import { apiUrl } from "../config.json";
 
 const apiEndPoint = apiUrl + "/movies";
 
@@ -7,6 +7,20 @@ export function getMovies() {
   return http.get(apiEndPoint);
 }
 
+export function getMovie(movieId) {
+  return http.get(apiEndPoint + "/" + movieId);
+}
+
 export function deleteMovies(movieId) {
   return http.delete(apiEndPoint + "/" + movieId);
+}
+
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(apiEndPoint + "/" + movie._id, body);
+  }
+
+  return http.post(apiEndPoint, movie);
 }
