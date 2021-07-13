0a82dd824d98ebcadf271b480e3ad66d73953550
Somade Daniel <Flexsidee@gmail.com>
2021-07-13 18:15:07 +0100

    Refactored movieService

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,23 +3,27 @@ import { apiUrl } from "../config.json";
 
 const apiEndPoint = apiUrl + "/movies";
 
+function movieUrl(id) {
+  return `${apiEndPoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndPoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndPoint + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function deleteMovies(movieId) {
-  return http.delete(apiEndPoint + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(apiEndPoint + "/" + movie._id, body);
+    return http.put(movieUrl(movie._id), body);
   }
 
   return http.post(apiEndPoint, movie);
