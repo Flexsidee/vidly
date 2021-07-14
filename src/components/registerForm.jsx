28f06ad202c4e5f9dc65673a34afce4a4ee33beb
Somade Daniel <Flexsidee@gmail.com>
2021-07-14 20:06:17 +0100

    Handled Register form submission and authentication to the backedn

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "./common/form";
 import Joi from "joi-browser";
+import * as userService from "../services/userService";
 
 class RegisterForm extends Form {
   state = {
@@ -18,9 +19,16 @@ class RegisterForm extends Form {
     name: Joi.string().required().label("Name"),
   };
 
-  doSubmit = () => {
-    //call the server
-    console.log("Submitted");
+  doSubmit = async () => {
+    try {
+      await userService.register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
