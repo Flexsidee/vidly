a1a6b8f4641060bcf2b118083d01e29fd651ad24
Somade Daniel <Flexsidee@gmail.com>
2021-07-14 20:22:54 +0100

    Handled Lofin form submission and authentication with the api

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,39 +1,47 @@
-import React from 'react';
-import Form from './common/form'
-import Joi from 'joi-browser';
+import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
+import { login } from "./../services/authService";
 
 class LoginForm extends Form {
-    state={
-        data: {
-            username: '',
-            password: ''
-        },
-        errors: {}
-    }
+  state = {
+    data: {
+      username: "",
+      password: "",
+    },
+    errors: {},
+  };
 
-    schema = {
-        username: Joi.string().required().label('Username'),
-        password: Joi.string().required().label('Password')
-    }
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
-
-    doSubmit = () =>{
-        //call the server
-        console.log('submitted');
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      await login(data.username, data.password);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
     }
+  };
 
-    render() {
-        return ( 
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={this.handleSubmit}>
-                    {this.renderInput('username', 'Username')}
-                    {this.renderInput('password', 'Password', 'password')}
-                    {this.renderButton('Login')}
-                </form>
-            </div>
-         );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default LoginForm;
