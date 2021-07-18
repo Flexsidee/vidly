059e26fee6a7a184fea023c5372784c1d413e2b6
Somade Daniel <Flexsidee@gmail.com>
2021-07-18 22:16:16 +0100

    Updated customers

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from "react";
 import Table from "./common/table";
+import Pagination from "./common/pagination";
+import SearchBox from "./common/searchBox";
 import {getCustomers }from "../services/customersService.js";
+import { paginate } from "../utils/paginate";
+import _ from "lodash";
 
 class Customers extends Component {
   state = {  
     customers: [],
+    currentPage: 1,
+    pageSize: 10,
     sortColumn: { path: "name", order: "asc" },
   }
 
@@ -20,20 +26,65 @@ class Customers extends Component {
     console.log(customers);
   }
 
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
+  };
+
   handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  getPageData = () => {
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      searchQuery,
+      customers: allCustomers,
+      sortColumn,
+    } = this.state;
+
+    let filtered = allCustomers;
+    if (searchQuery)
+      filtered = allCustomers.filter((m) =>
+        m.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+   
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const customers = paginate(sorted, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: customers };
+  };
   
   render() { 
-    const { onSort, sortColumn, customers } = this.state;
+    const { pageSize, currentPage, onSort, sortColumn } = this.state;
+    
+    const { totalCount, data: customers } = this.getPageData();
     
     return (
-      <Table
-        onSort={this.handleSort}
-        columns={this.columns}
-        data={customers}
-        sortColumn={sortColumn}
-      />
+      <React.Fragment>
+        <SearchBox
+          value={this.state.searchQuery}
+          onChange={this.handleSearch}
+        />
+        <Table
+          onSort={this.handleSort}
+          columns={this.columns}
+          data={customers}
+          sortColumn={sortColumn}
+        />
+        <Pagination
+          itemsCount={totalCount}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
     );
   }
 }
