f0739b3f44286c241a256f860762a9b9d7d1b706
Somade Daniel <Flexsidee@gmail.com>
2021-07-20 05:12:23 +0100

    Redirect user after Login to last location

--- src/components/common/protectedRoute.jsx
+++ src/components/common/protectedRoute.jsx
@@ -7,7 +7,15 @@ const ProtectedRoute = ({ component: Component, render, ...rest }) => {
     <Route
       {...rest}
       render={(props) => {
-        if (!auth.getCurrentUser()) return <Redirect to="/login" />;
+        if (!auth.getCurrentUser())
+          return (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: props.location },
+              }}
+            />
+          );
         return Component ? <Component {...props} /> : render(props);
       }}
     />
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import Form from "./common/form";
 import Joi from "joi-browser";
 import auth from "../services/authService";
@@ -21,7 +22,9 @@ class LoginForm extends Form {
     try {
       const { data } = this.state;
       await auth.login(data.username, data.password);
-      window.location = "/";
+
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
@@ -32,6 +35,8 @@ class LoginForm extends Form {
   };
 
   render() {
+    if (auth.getCurrentUser()) return <Redirect to="/" />;
+
     return (
       <div>
         <h1>Login</h1>
